fc9d9ab06817a003d0a5be0d889d7fee74908a27
caowei <solocao@126.com>
2018-10-30 21:21:09 +0800

    配置tab

---
+++ src/pages/market/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+    market
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/my/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+    my
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
