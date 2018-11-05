ce74cfb7e4de161c6e9551c391ca808decb2b6a2
caowei <solocao@126.com>
2018-11-05 09:42:19 +0800

    wux组件已经可以使用了

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="detail">
+    <wux-cell-group>
+      <wux-cell hover-class="none">
+        <div class="flex-space-between">
+          <span>预计收益</span>
+          <span>***(身份认证后查看收益)</span>
+        </div>
+      </wux-cell>
+      <wux-cell hover-class="none">
+        <div class="flex-space-between">
+          <span>广告类型</span>
+          <span>车内+车外</span>
+        </div>
+      </wux-cell>
+      <wux-cell hover-class="none">
+        <div class="flex-space-between">
+          <span>收益时间</span>
+          <span>08月12日 ～ 09月11日</span>
+        </div>
+      </wux-cell>
+      <wux-cell hover-class="none">
+        <div class="flex-space-between">
+          <span>安装时间</span>
+          <span>08月12日 ～ 09月11日</span>
+        </div>
+      </wux-cell>
+      <wux-cell hover-class="none">
+        <div class="flex-space-between">
+          <span>车辆要求</span>
+          <span>南京市车辆</span>
+        </div>
+      </wux-cell>
+      <wux-cell hover-class="none">
+        <div class="flex-space-between">
+          <span>车辆颜色</span>
+          <span>黑色、粉色、银色、蓝色、紫色</span>
+        </div>
+      </wux-cell>
+      <wux-cell hover-class="none">
+        <wux-input label="WeChat" />
+      </wux-cell>
+      <wux-cell hover-class="none">
+        <div class="flex-space-between">
+          <span>sf</span>
+          <span>dsf</span>
+        </div>
+      </wux-cell>
+      <wux-cell hover-class="none">
+        <wux-input label="Password" placeholder="Input your password" password type="number" />
+      </wux-cell>
+    </wux-cell-group>
+  </div>
+</template>
+<style lang="less" scoped>
+.detail {
+  font-size: 14px;
+  color: #656665;
+}
+
+.flex-space-between {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
+
